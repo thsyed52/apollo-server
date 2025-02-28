1a2262903c41935a497ecf70d2d1fc1a5b0844af
Syed Ali <thsyed52@yahoo.com>
2025-02-27 21:21:46 -0500

    changes to index.js server code added

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+
+// server setup
+
+const server = new ApolloServer({
+    // takes 2 arguments
+
+    // typedefinations
+
+    // resolvers
+})
+
+const {url} = await startStandaloneServer( server , {
+    listen: {port: 4000}
+})
+
+console.log('Server listening at port ' , 4000)
