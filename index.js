454c2909c2afb6cb6c410137bb5c68b49fa66f23
Syed Ali <thsyed52@yahoo.com>
2025-03-07 09:28:45 -0500

    some changes

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { typedefinations } from './schema';
+import { typedefinations } from './schema.js';
+import db from './dummydb.js'
 
+
+const resolvers = {
+    Query: {
+        games() { return db.games },
+        reviews() { return db.reviews },
+        authors() { return db.authors },
+
+    }
+}
 
 // server setup
 
@@ -9,7 +19,7 @@ const server = new ApolloServer({
     // takes 2 arguments
     typedefinations,
     // typedefinations = definations of type of data which the user can query like schema
-
+    resolvers
     // resolvers = how to solve the queries done on schema / typedefinations
 })
 
--- schema.js
+++ schema.js
@@ -5,26 +5,22 @@ type Game {
     title: String!
     platform: [String!]!
 }
-
 type Review {
     id: ID!
     rating: Int!
     content: String!
 }
-
 type Author {
     id: ID!
     name: String!
     verified: Boolean!
 }
-
 type Query {
     reviews: [Review]
     games: [Game]
     authors: [Author]
 }
-
-`
+`;
 
 // Query type is a must . Every GraphQL schema has to have it. Its job is to define 
 // entry points in the graph and specific the return types of those entry points
